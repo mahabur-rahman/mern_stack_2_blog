5b047ab0174af069005946a915cb7b691ba9e4ba
mahabur  rahman <annur4395@gmail.com>
2022-11-01 18:10:26 +0600

    image upload in server using multer

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const colors = require("colors");
+const multer = require("multer");
 const dotenv = require("dotenv");
 // ROUTE
 const authRoute = require("./routes/auth");
@@ -15,6 +16,26 @@ const PORT = process.env.PORT || 5000;
 // connected to db
 const connectedDB = require("./db/connect");
 connectedDB();
+
+// ######## UPLOAD IMAGE  ########
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+
+  filename: (req, file, cb) => {
+    // cb(null, "hello.jpg");
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  return res.status(200).json("File has been uploaded!");
+});
+
+// ######## END OF UPLOAD IMAGE  ########
 
 // middleware
 app.use(express.json());
