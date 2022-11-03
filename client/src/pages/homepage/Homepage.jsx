31c60ff99b181d638b885101318aa31059b4c329
mahabur  rahman <annur4395@gmail.com>
2022-11-03 15:50:05 +0600

    get single post using query

--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -4,21 +4,24 @@ import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./homepage.css";
 import axios from "axios";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Homepage() {
   const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
 
   useEffect(() => {
     const getPosts = async () => {
       try {
-        const res = await axios.get(`/posts`);
+        const res = await axios.get(`/posts` + search);
         setPosts(res.data);
+        console.log(res.data);
       } catch (err) {
         console.log(err);
       }
     };
     getPosts();
-  }, []);
+  }, [search]);
 
   return (
     <>
